541fd3a482912f22f037b37f6e0035cc52a7ad4d
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-28 22:28:00 +0700

    fix kesalahan di Person.java, saat di personApp.java tidak dapat null pada object person3

--- src/latihan/Person.java
+++ src/latihan/Person.java
@@ -5,7 +5,19 @@ public class Person {
     String address;
     final String country = "Indonesia";
 
+    /**
+     * dibawah ini merupakan code constructor:
+     * sebuah method yang otomatis dipanggil saat object dibuat.
+     *
+     * @param paramName
+     * @param paramAddress
+     */
+    Person(String paramName, String paramAddress) {
+        name = paramName;
+        address = paramAddress;
+    }
+
     void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + ", My name is " + paramName);
+        System.out.println("Hello " + paramName + ", My name is " + name);
     }
 }
--- src/latihan/PersonApp.java
+++ src/latihan/PersonApp.java
@@ -3,9 +3,9 @@ package latihan;
 public class PersonApp {
     public static void main(String[] args) {
         // object 1
-        var person1 = new Person();
-        person1.name = "Ayu";
-        person1.address = "Setu";
+        var person1 = new Person("Ayu", "Setu");
+        /*person1.name = "Ayu";
+        person1.address = "Setu";*/
 
         System.out.println(person1.name);
         System.out.println(person1.address);
@@ -14,16 +14,12 @@ public class PersonApp {
         person1.sayHello("Rahayu");
 
         // object 2
-        Person person2 = new Person();
+        Person person2 = new Person("Puji", "Cideng");
 
         // object 3
         Person person3;
-        person3 = new Person();
+        person3 = new Person("Astuty","Kuningan");
 
-        /**
-         * harusnya code dibawah null kalo belum ada person3.name = "Ayu" -> contoh
-         * tapi kenapa tetep tampil Puji guys? saya tak paham lhaa :(
-         */
         person3.sayHello("Puji");
     }
 }
