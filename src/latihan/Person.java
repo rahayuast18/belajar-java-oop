4f4744d0ff8acd807aee9d479b06292a6993fa3e
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-28 22:34:58 +0700

    done for constructor and this

--- src/latihan/Person.java
+++ src/latihan/Person.java
@@ -9,15 +9,23 @@ public class Person {
      * dibawah ini merupakan code constructor:
      * sebuah method yang otomatis dipanggil saat object dibuat.
      *
-     * @param paramName
-     * @param paramAddress
+     * @param name
+     * @param address
+     *
+     * this:
+     * jika di .java lain ingin memanggil attribute dari class Person,
+     * harus membuat object dulu seperti "var person = new Person();".
+     * baru bisa akses -> person.name
+     * nah, this itu seperti person -> hanya saja digunakan dalam class yang sama.
+     * menunjukkan suatu attribute dalam 1 class yang sama
+     * (this untuk mengakses object saat ini)
      */
-    Person(String paramName, String paramAddress) {
-        name = paramName;
-        address = paramAddress;
+    Person(String name, String address) {
+        this.name = name;
+        this.address = address;
     }
 
-    void sayHello(String paramName) {
-        System.out.println("Hello " + paramName + ", My name is " + name);
+    void sayHello(String name) {
+        System.out.println("Hello " + name + ", My name is " + this.name);
     }
 }
