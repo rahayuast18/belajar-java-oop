03593dfb63480d4847f073b0b8d13bca9a760db4
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-29 08:00:22 +0700

    Revert "fix comment this.name in sayHello VicePresident.java & try to access parent with 'super'"
    This reverts commit 08483c42a414c9ed46c690744f59b5b1111522a5.

--- src/latihan/VicePresident.java
+++ src/latihan/VicePresident.java
@@ -1,27 +1,42 @@
 package latihan;
 
 class VicePresident extends Manager {
+    /**
+     * karena di parent ada constructor, maka di child harus dipanggil walaupun diisi null
+     *
+     * @param company
+     */
+    VicePresident(String company) {
+        super(company);
+    }
+
     public static void main(String[] args) {
         /**
          * how to access parent & child
          */
-        var manager = new Manager();
+        var manager = new Manager("Indocyber");
         manager.name = "Arum";
         manager.sayHello("Denanda");
 
-        var vicePresident = new VicePresident();
+        var vicePresident = new VicePresident("Anteraja");
         vicePresident.name = "Jerry";
         /**
          * jika tidak ada sayHello pada class VicePresident -> yang ditampilkan adalah method dari manager
          * jika ada method yang sama di child dengan di parent -> yang dipanggil adalah method dari child
          */
         vicePresident.sayHello("Denanda");
+
+        /**
+         * how to call & use super constructor
+         */
+        vicePresident.sayName("Ayu");
     }
 
     void sayHello(String name) {
-        /**
-         * this.name from class Manager
-         */
         System.out.println("Hello " + name + ", I'm VP " + this.name);
+    }
+
+    void sayName(String name) {
+        System.out.println("Hello " + name + ", I'm from " + super.company + " and he's from " + this.company);
     }
 }
