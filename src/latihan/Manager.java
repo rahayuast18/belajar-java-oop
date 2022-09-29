1383926aa19a63fd64be50bd0b7f2ffcee637b59
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-29 07:43:46 +0700

    fix comment this.name in sayHello VicePresident.java

--- src/latihan/Manager.java
+++ src/latihan/Manager.java
@@ -3,6 +3,9 @@ package latihan;
 class Manager {
     String name;
     String company;
+    Manager(String company) {
+        this.company = company;
+    }
 
     void sayHello(String name) {
         System.out.println("Hello " + name + ", I'm Manager " + this.name);
