08483c42a414c9ed46c690744f59b5b1111522a5
Rahayu Puji Astuty <rahayupujiastuty1@gmail.com>
2022-09-29 07:46:11 +0700

    fix comment this.name in sayHello VicePresident.java & try to access parent with 'super'
    This reverts commit 1383926aa19a63fd64be50bd0b7f2ffcee637b59.

--- src/latihan/Manager.java
+++ src/latihan/Manager.java
@@ -3,9 +3,6 @@ package latihan;
 class Manager {
     String name;
     String company;
-    Manager(String company) {
-        this.company = company;
-    }
 
     void sayHello(String name) {
         System.out.println("Hello " + name + ", I'm Manager " + this.name);
